2fc0c9f6bbf86adde17224a22b1915cc75943930
Mehmed Faheim Arslan <mehmedfaheimarslan@gmail.com>
2025-11-15 16:02:34 +0000

    input arg setup

--- wc/src/main.cpp
+++ wc/src/main.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 #include <string>
+#include <format>
+#include <vector>
 
 #include "wc.hpp"
 
 int main(int argc, char* argv[]) {
     std::string dataPath = "../../data/";
-    std::string fileName;
+    std::string fileName, completeFilePath;
+    int op = 0;
     if (argc > 0) {
-        fileName = dataPath + argv[2];
+        fileName = argv[2];
+        completeFilePath = dataPath + fileName;
+        std::vector<std::string> args(argv + 1, argv + argc);
+       
+        for (const auto& arg: args) {
+            if (arg == "-c") {
+                op = count_bytes(completeFilePath);
+            }   
+        }
     }
 
-    std::cout << count_bytes(fileName) << std::endl;
+    std::cout << std::format("{} {}", op, fileName) << std::endl;
     return 0;
 }
