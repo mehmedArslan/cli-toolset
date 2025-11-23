323e82dd1e51f6fad243d711d6ad39413f5dc697
Mehmed Faheim Arslan <mehmedfaheimarslan@gmail.com>
2025-11-23 21:58:34 +0000

    update cmake, remove bat files

--- wc/debug.bat
+++
@@ -1,7 +0,0 @@
-echo 'Release build and run'
-cd "build"
-cmake ".."
-cmake --build . --config Debug 
-cd Debug 
-wc.exe test.txt
-pause
--- wc/run.bat
+++
@@ -1,6 +0,0 @@
-echo 'Release build and run'
-cd "build"
-cmake ".."
-cmake --build . --config Release
-cd Release
-wc.exe test.txt
